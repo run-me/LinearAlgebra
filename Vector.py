fe5fe1215ac7ab2bd5a41aa9700ab93133bf65c0
mash-tensor <wys925@my.utsa.edu>
2019-11-09 19:16:10 -0600

    vector operations added

--- Vector.py
+++ Vector.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 class Vector(object):
     def __init__(self, coordinates):
         try:
@@ -15,3 +18,34 @@ class Vector(object):
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+
+    def vector_sum(self, v):
+        assert v.dimension == self.dimension, "Dimensions should be same to perform vector sum"
+        vector_sum = [x + y for x, y in zip(self.coordinates, v.coordinates)]
+        return Vector(vector_sum)
+
+    def vector_diff(self, v):
+        assert v.dimension == self.dimension, "Dimensions should be same to perform vector subtraction"
+        vector_difference = [x - y for x, y in zip(self.coordinates, v.coordinates)]
+        return Vector(vector_difference)
+
+    def scale_vector(self, scale):
+        vector_scaled = [scale * coordinates for coordinates in self.coordinates]
+        return Vector(vector_scaled)
+
+    def magnitude(self):
+        coordinates_squared = [x**2 for x in self.coordinates]
+        return sqrt(sum(coordinates_squared))
+
+    def normalize(self):
+        vec_mag = self.magnitude()
+        unit_vector = [x / vec_mag for x in self.coordinates]
+        return Vector(unit_vector)
+
+
+v1 = Vector([5.581, -2.136])
+print(v1.magnitude())
+print(v1.normalize().coordinates)
+v2 = Vector([1.996, 3.108, -4.554])
+print(v2.magnitude())
+print(v2.normalize().coordinates)
