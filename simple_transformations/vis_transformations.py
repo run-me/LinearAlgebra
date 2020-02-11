ba2427fdeaa5af7306de609ec63ede5e0264e777
Maheshwaran Umapathy <wys925@my.utsa.edu>
2020-02-11 15:00:08 -0600

    Hardening implementation, added matrix dims validation

--- simple_transformations/vis_transformations.py
+++ simple_transformations/vis_transformations.py
@@ -1,25 +1,22 @@
 import matplotlib.pyplot as plt
-import matplotlib.ticker as plticker
 import numpy as np
 
 
 def visualize_transformation(vector, trans_matrix):
     """
-    :param vector:
-    :param trans_matrix:
-    :return:
+        Method to visualize the transform operation on a vector.
+    :param vector: input vector as a list or np.nd array
+    :param trans_matrix: transform matrix as np.nd array
+    :return: None
     """
     fig, ax = plt.subplots()
-    mapped_vector = np.dot(trans_matrix, vector)
+    try:
+        mapped_vector = np.dot(trans_matrix, vector)
+    except ValueError:
+        raise ValueError("check input vector and transform matrix dimensions")
     
     ax.quiver(0, 0, vector[0], vector[1], color='blue', angles='xy', scale_units='xy', scale=1, label="input vector")
-    
-    ax.quiver(0, 0,
-              mapped_vector[0], mapped_vector[1],
-              angles='xy',
-              scale_units='xy',
-              color='green',
-              scale=1,
+    ax.quiver(0, 0, mapped_vector[0], mapped_vector[1], angles='xy', scale_units='xy', color='green', scale=1,
               label="transformed vector")
     
     # Add the grid
@@ -37,7 +34,7 @@ if __name__ == "__main__":
     # construct the vector
     inp_vector = np.array([1, 1])
     # construct the transformation matrix
-    transform_matrix = np.array([[-1, 0], [0, 1]])
+    transform_matrix = np.array([[-1]])
     
     # call visualization method to visualize transformation
     visualize_transformation(inp_vector, transform_matrix)
