4c9947100fb98a476508e750b18f9a2b59bbfc03
Maheshwaran Umapathy <wys925@my.utsa.edu>
2020-02-11 14:53:28 -0600

    Finally got it right :) Yay!

--- simple_transformations/vis_transformations.py
+++ simple_transformations/vis_transformations.py
@@ -12,23 +12,30 @@ def visualize_transformation(vector, trans_matrix):
     fig, ax = plt.subplots()
     mapped_vector = np.dot(trans_matrix, vector)
     
-    ax.quiver(0, 0, vector[0], vector[1], color='red', angles='xy', scale_units='xy', scale=1, label="input vector")
-    # ax.quiver(0, 0, mapped_vector[0], mapped_vector[1], color='green', scale=.1, label="transformed vector")
+    ax.quiver(0, 0, vector[0], vector[1], color='blue', angles='xy', scale_units='xy', scale=1, label="input vector")
+    
+    ax.quiver(0, 0,
+              mapped_vector[0], mapped_vector[1],
+              angles='xy',
+              scale_units='xy',
+              color='green',
+              scale=1,
+              label="transformed vector")
     
     # Add the grid
     ax.grid(which='major', axis='both', linestyle='-')
     
-    ax.set_xlim(-5, 5)
-    ax.set_ylim(-5, 5)
-    ax.set_ylabel("y axis")
-    ax.set_xlabel("x axis")
+    ax.set_xlim(-4, 4)
+    ax.set_ylim(-4, 4)
+    ax.set_ylabel("y axis", color='red')
+    ax.set_xlabel("x axis", color='red')
     ax.legend()
     plt.show()
 
 
 if __name__ == "__main__":
     # construct the vector
-    inp_vector = np.array([.2, .2])
+    inp_vector = np.array([1, 1])
     # construct the transformation matrix
     transform_matrix = np.array([[-1, 0], [0, 1]])
     
